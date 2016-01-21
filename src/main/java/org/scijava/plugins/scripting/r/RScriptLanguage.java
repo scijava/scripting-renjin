a7f1157bfe0a964554b99ee517540fad90e342ef
Mark Hiner <hinerm@gmail.com>
2016-01-21 03:13:14 -0600

    Use Renjin instead of RServe
    Renjin is a complete R implementatin in Java. They provide a JSR223
    script engine, which is preferrable to connecting to a running RServe
    instance.
    
    This represents a new direction for R scripting in Java. Given that this
    is a Java re-implementation of R, this project will from now on be
    "scripting-renjin" - allowing for the possibility of a true scripting-r
    implementation in the future.

--- src/main/java/org/scijava/plugins/scripting/r/RScriptLanguage.java
+++ src/main/java/org/scijava/plugins/scripting/r/RScriptLanguage.java
@@ -1,20 +1,20 @@
 /*
  * #%L
- * JSR-223-compliant R scripting language plugin.
+ * SciJava wrapper around the Renjin R implementation.
  * %%
- * Copyright (C) 2011 - 2015 Board of Regents of the University of
+ * Copyright (C) 2015 - 2016 Board of Regents of the University of
  * Wisconsin-Madison.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -28,19 +28,21 @@ import java.util.List;
 
 import javax.script.ScriptEngine;
 
-import org.rosuda.REngine.REXP;
+import org.renjin.script.RenjinScriptEngineFactory;
 import org.scijava.plugin.Plugin;
 import org.scijava.script.AbstractScriptLanguage;
 import org.scijava.script.ScriptLanguage;
 
 /**
  * An adapter of the R interpreter to the SciJava scripting interface.
- * 
+ *
  * @author Curtis Rueden
  * @see ScriptEngine
  */
 @Plugin(type = ScriptLanguage.class, name = "R")
 public class RScriptLanguage extends AbstractScriptLanguage {
+
+	private final RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
 
 	@Override
 	public List<String> getExtensions() {
@@ -49,20 +51,20 @@ public class RScriptLanguage extends AbstractScriptLanguage {
 
 	@Override
 	public String getEngineName() {
-		return "Rserve";
+		return factory.getEngineName();
 	}
 
 	@Override
 	public ScriptEngine getScriptEngine() {
-		return new RScriptEngine(getContext());
+		return factory.getScriptEngine();
 	}
 
 	@Override
 	public Object decode(final Object object) {
-		if (object instanceof REXP) {
-			return RUtils.getVar((REXP) object);
-		}
-		throw new IllegalArgumentException("Object is not an R expression");
+		// TODO if externalptr need to convert back to Java object
+		if (object instanceof SEXP)
+			return RUtils.getJavaValue((SEXP) object);
+		return object;
 	}
 
 }
--- src/test/java/org/scijava/plugins/scripting/r/RTest.java
+++ src/test/java/org/scijava/plugins/scripting/r/RTest.java
@@ -1,20 +1,20 @@
 /*
  * #%L
- * JSR-223-compliant R scripting language plugin.
+ * SciJava wrapper around the Renjin R implementation.
  * %%
- * Copyright (C) 2011 - 2015 Board of Regents of the University of
+ * Copyright (C) 2015 - 2016 Board of Regents of the University of
  * Wisconsin-Madison.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -36,7 +36,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
 import org.junit.Test;
-import org.rosuda.REngine.REXP;
+import org.renjin.script.RenjinScriptEngine;
 import org.scijava.Context;
 import org.scijava.script.ScriptLanguage;
 import org.scijava.script.ScriptModule;
@@ -50,9 +50,7 @@ import org.scijava.script.ScriptService;
 public class RTest {
 
 	@Test
-	public void testBasic() throws InterruptedException, ExecutionException,
-		IOException, ScriptException
-	{
+	public void testBasic() throws InterruptedException, ExecutionException, IOException, ScriptException {
 		final Context context = new Context(ScriptService.class);
 		final ScriptService scriptService = context.getService(ScriptService.class);
 		final String script = "as.integer(1) + as.integer(2)";
@@ -62,15 +60,28 @@ public class RTest {
 	}
 
 	@Test
-	public void testString() throws InterruptedException, ExecutionException,
-		IOException, ScriptException
-	{
+	public void testString() throws InterruptedException, ExecutionException, IOException, ScriptException {
 		final Context context = new Context(ScriptService.class);
 		final ScriptService scriptService = context.getService(ScriptService.class);
-		final String script = "result = R.version.string\n";
+		final String script = "result = R.version$version.string\n";
 		final ScriptModule m = scriptService.run("version.r", script, true).get();
 		final Object result = m.getReturnValue();
-		assertTrue(result.toString().startsWith("R version "));
+		assertTrue(result.toString().startsWith("Renjin version "));
+	}
+
+	@Test
+	public void testSciJava() throws InterruptedException, ExecutionException, IOException, ScriptException {
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+		final String script = "" + //
+				"# @OUTPUT org.scijava.plugins.scripting.r.RScriptLanguage language\n" + //
+				"import(org.scijava.plugins.scripting.r.RScriptLanguage)\n" + //
+				"language <<- RScriptLanguage$new()\n" + //
+				"print(language$engineName)\n";
+		final ScriptModule m = scriptService.run("sjc.r", script, true).get();
+
+		final Object actual = m.getOutput("language");
+		assertEquals(scriptService.getLanguageByName("R").getClass(), actual.getClass());
 	}
 
 	@Test
@@ -80,35 +91,32 @@ public class RTest {
 
 		final ScriptLanguage language = scriptService.getLanguageByExtension("r");
 		final ScriptEngine engine = language.getScriptEngine();
-		assertEquals(RScriptEngine.class, engine.getClass());
+		assertEquals(RenjinScriptEngine.class, engine.getClass());
 		engine.put("hello", 17);
-		assertEquals(17, RUtils.getVar((REXP) engine.eval("hello")));
-		assertEquals(17, RUtils.getVar((REXP) engine.get("hello")));
+		assertEquals(17, RUtils.getJavaValue((SEXP) engine.eval("hello")));
+		assertEquals(17, RUtils.getJavaValue((SEXP) engine.get("hello")));
 
 		final Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
 		bindings.clear();
-		assertNull(engine.get("hello"));
+		assertNull(RUtils.getJavaValue((SEXP) engine.get("hello")));
+		assertNull(RUtils.getJavaValue((SEXP) engine.get("polar_kraken")));
 	}
 
-	//FIXME currently unable to use @Parameters as the default RConnection
-	// does not support callign methods of java classes...
-//	@Test
-//	public void testParameters() throws InterruptedException, ExecutionException,
-//		IOException, ScriptException
-//	{
-//		final Context context = new Context(ScriptService.class);
-//		final ScriptService scriptService = context.getService(ScriptService.class);
-//
-//		final String script = "" + //
-//			"# @ScriptService ss\n" + //
-//			"# @OUTPUT String language\n" + //
-//			"language = ss.getLanguageByName('R').getLanguageName()\n";
-//		final ScriptModule m = scriptService.run("hello.r", script, true).get();
-//
-//		final Object actual = m.getOutput("language");
-//		final String expected =
-//			scriptService.getLanguageByName("R").getLanguageName();
-//		assertEquals(expected, actual);
-//	}
+	@Test
+	public void testParameters() throws InterruptedException, ExecutionException, IOException, ScriptException {
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+
+		final String script = "" + //
+				"# @ScriptService ss\n" + //
+				"# @OUTPUT String name\n" + //
+				"language <- ss$getLanguageByName('R')\n" + //
+				"name <- language$languageName\n";
+		final ScriptModule m = scriptService.run("hello.r", script, true).get();
+
+		final Object actual = m.getOutput("name");
+		final String expected = scriptService.getLanguageByName("R").getLanguageName();
+		assertEquals(expected, actual);
+	}
 
 }
